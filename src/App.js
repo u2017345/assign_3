46136b6d6e226a03b119669cec16e4daec2a0dcb
u2017345 <73095165+u2017345@users.noreply.github.com>
2020-11-06 11:37:30 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello, hello, 345 123
+          hello, Deploy done
         </p>
         <a
           className="App-link"
